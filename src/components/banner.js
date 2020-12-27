0e33945c55192a9779bbdf4689bcd05ac777205a
FabricioMF72 <55679079+FabricioMF72@users.noreply.github.com>
2020-12-27 17:27:45 -0600

    [ADD] banner component

---
+++ src/components/banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const banner = () => {
+    return (
+        <div className="banner">
+            <img src="/imgs/GRADIENT2-100.jpg" alt="gradiant" className="banner-img"/>
+        </div>
+    )
+}
+
+export default banner
