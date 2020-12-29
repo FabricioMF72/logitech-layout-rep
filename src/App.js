ccc9f0e4ccde4d8b0d386a40f1aa44faa9caad17
FabricioMF72 <55679079+FabricioMF72@users.noreply.github.com>
2020-12-28 18:36:50 -0600

    [ADD] banner items

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Header from "./components/Header"
 function App() {
   return (
     <div className="app">
-      <Banner/>
       <Header/>
+      <Banner/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,20 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'react-bootstrap/Image'
 const Header = () => {
     return ( 
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="header-c">
+        <Navbar collapseOnSelect expand="lg" className="header-c">
+            <Navbar.Brand href="#home">
+                <Image className="header-logo" src="./imgs/LOGO.png" fluid />
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto navbar-items">
-                    <Nav.Link className="navbar-item" href="#features">Support the team</Nav.Link>
-                    <Nav.Link className="navbar-item" href="#pricing">Custom</Nav.Link>
-                    <Nav.Link className="navbar-item" href="#features">Support the team</Nav.Link>
-                    <Nav.Link className="navbar-item" href="#pricing">More</Nav.Link>
+                    <Nav.Link className="navbar-item" href="#features"> <h1 className="navbar-text">Support the team</h1> </Nav.Link>
+                    <Nav.Link className="navbar-item" href="#pricing"><h1 className="navbar-text">Custom</h1></Nav.Link>
+                    <Nav.Link className="navbar-item" href="#features"><h1 className="navbar-text">Support the team</h1></Nav.Link>
+                    <Nav.Link className="navbar-item" href="#pricing"><h1 className="navbar-text">More</h1></Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
